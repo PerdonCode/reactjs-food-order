a9511bde06522c7b12055c3875dba510c6f63881
PerdonCode <niels.perdon1@gmail.com>
2023-06-13 12:18:22 +0200

    added cart module

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart/Cart'
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,12 +17,13 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
+      {cartIsShown && <div>Cart....</div>}
+      <Cart />
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </CartProvider>
+    </CartProvider> 
   );
 }
 
